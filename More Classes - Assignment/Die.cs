b4279f642f2c33e0760f59272379ff0dd571bdab
Raihan Carder <339509226@GoTVDSB.ca>
2023-09-21 10:59:34 -0400

    Add project files.

---
+++ More Classes - Assignment/Die.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    public class Die
+    {
+        // Create fields
+        private int _roll;
+        private Random _generator;
+
+        // Create Constructor, Lets the variables acc work, and lets it store data.
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+        }
+
+        // Accessor property, when defining property make it Uppercase Letter ex: Roll instead of roll or _roll
+
+        public int Roll
+        {
+            get { return _roll; } // only get because they can only retrieve value and user cant change value. add "Set" to change.
+        }
+
+        // decides what is printed when toString is called
+        // Public means can code can be used outside of class private means only in class
+
+        public override string ToString()
+        {
+            return _roll + "";
+        }
+
+        public int RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+            return _roll;
+        }
+
+        public void DrawRoll()
+        {
+            switch (_roll)
+            {
+                case 1:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine(" ---");
+                    break;
+                case 2:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o  |");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|  o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 3:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o  |");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|  o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 4:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|   |");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+                case 5:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("| o |");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+                    break;
+                case 6:
+                    Console.WriteLine(" ---");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine("|o o|");
+                    Console.WriteLine(" ---");
+                    break;
+            }
+        }
+    }
+}
---
+++ More Classes - Assignment/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   // Raihan Carder
+
+            string name;
+            int selection = 0;
+            double bank = 100, betAmount = 0;
+            bool validSelection = false;
+            Die die1 = new Die();
+            Thread.Sleep(1000);
+            Die die2 = new Die();
+            die1.RollDie();
+            die2.RollDie();
+            Console.Title = "Gambling Game";
+
+            Console.WriteLine("Casino");
+            Console.WriteLine();
+            Console.Write("Enter your Name:");
+            name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name}, Today we're going to be rolling dies!");
+            Console.WriteLine("What you can bet on includes: Doubles, Not Doubles, Even sum or Odd sum.") ;
+            Console.WriteLine($"You starting amount is ${bank}!");
+            Console.WriteLine("Type the number corresponding with your game!");
+            Console.WriteLine("1. Doubles: Win double yout bet!");
+            Console.WriteLine("2. Not Doubles: Win half your bet!");
+            Console.WriteLine("3. Even Sum: Win your bet!");
+            Console.WriteLine("4. Odd Sum: Win your bet!");
+            Console.Write("What game would you like to play?");
+            while (validSelection == false)
+            {
+                if (int.TryParse(Console.ReadLine(), out selection))
+                {                   
+                    if (selection == 1)
+                    {
+                        validSelection = true;
+                    }
+                    else if (selection == 2)
+                    {
+                        validSelection = true;
+                    }
+                    else if (selection == 3)
+                    {
+                        validSelection = true;
+                    }
+                    else if (selection == 4)
+                    {
+                        validSelection = true;
+                    }
+                    else
+                    {
+                        Console.Write("Error, what game would you like to play: ");
+                    }
+                }
+                else
+                {
+
+                }
+            }
+
+            Console.Write("Enter your bet amount: ");
+            if (Double.TryParse(Console.ReadLine(), out betAmount))
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+            if (selection == 1)
+            {
+                Console.WriteLine("Welcome to the Doubles table, Your rolls are....");
+                Thread.Sleep(1000);
+                die1.DrawRoll();
+                Thread.Sleep(1000);
+                die2.DrawRoll();
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
